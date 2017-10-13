8f1c128a16b23bdafe39d331837d7466faf168d8
casperinous <nikostotosis@gmail.com>
2017-10-13 06:48:29 +0300

    Early example of hooking based on a json configuration

--- jni/ronin/ronin.c
+++ jni/ronin/ronin.c
@@ -9,9 +9,14 @@
 #include "sharedlib.h"     
 #include <sys/socket.h>
 #include <jni.h>
+#include <android/asset_manager.h>
 #include "elfutils.h"
 #include "log.h"
 #include "hook.h"
+#include "storage.h"
+#include "config.h"
+#include "app.h"
+#include "sll.h"
 
 unsigned int mconnect(int socket, const struct sockaddr *address, socklen_t address_len)
 {
@@ -57,6 +62,8 @@ jint JNICALL JNI_OnLoad(JavaVM* vm, void* aReserved)
 	}
 	
 	return JNI_VERSION_1_6;
+
+	LOGX(INFO, "Size of JavaVM struct is %zu", sizeof(JavaVM));
 }
 
 
@@ -68,6 +75,49 @@ void __attribute__ ((constructor)) ronin_main()
 	
 	LOGX(INFO, "%s","[*]---------------------------------------------");
 	LOGX(INFO, "%s","[*] Starting Ronin - Test example targeting libc and connect function.");	
+	
+	
+	char * path = get_external_storage();
+	if( path )
+	{
+		app * ap = get_app_info();
+		if( ap->appname )
+		{
+			LOGX(INFO,"[*] App's name :> %s", ap->appname);
+			char * app_public_folder = get_app_public_path(path, ap->appname);
+			if( app_public_folder  )
+			{
+				char buffer[4096], path[2048];         
+				FILE *fp = NULL;         
+				sprintf( path , "%s%s",app_public_folder, "config.json" );
+				LOGX(INFO,"[*] Path of the config file :> %s", path );
+				fp = fopen(path, "r+");
+				if( fp )
+				{
+					int result = fread (buffer, 1, 4096, fp);
+					LOGX(INFO,"[*] We read %d bytes and content is %s", result, buffer);
+					config* conf = parse_from_file(buffer);
+					slinked_node_t * tmp = conf->hooks->head;
+					while( tmp != NULL) 
+					{      	
+					   	hook * htmp = (hook *)tmp->data;
+						LOGX(INFO,"[+] Got Hook at address [%p] with data :> %s %s %s %s", htmp, htmp->func_name, htmp->exp_name, htmp->trg_so, htmp->exp_so);	
+						tmp = tmp->next;
+						break; 
+					}
+					LOGX(INFO,"%s","[*] Finished printing hooks. Exiting .....");
+
+				}
+				fclose(fp);
+			}
+		}
+	}
+
+
+	
+	
+	
+	
 	shared_lib *lib = find_shared_lib("libc.so");
 
     	if( lib == NULL)
@@ -75,16 +125,16 @@ void __attribute__ ((constructor)) ronin_main()
     		LOGX(ERROR,"[-] Could not find %s", "libc.so" );
     	} else 
 	{
+		/*
 		unsigned int (*m_con)(int socket, const struct sockaddr *address, socklen_t address_len) = mconnect;
 		unsigned long *val = (unsigned long *)&m_con;
-		hook *hk = init_hook_str(*val,"connect","libc.so","");
+		hook *hk = init_hook_str(*val,"connect","m_con","libc.so","");
 		if( hk )
 		{	
 			hook_func(lib,hk);
-		}
+		}*/
 	}
 
 	LOGX(INFO, "%s","[*]---------------------------------------------");
-
-
+	
 }
